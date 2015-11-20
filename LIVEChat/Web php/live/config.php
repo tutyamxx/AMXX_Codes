6f5b0916a0ac59836f388121e8c19b5d6a3a2375
tutyamxx <tutygoo@gmail.com>
2015-11-20 14:08:45 +0000

    Update config.php

--- LIVEChat/Web php/live/config.php
+++ LIVEChat/Web php/live/config.php
@@ -5,10 +5,10 @@ error_reporting(E_ALL);
 
 // Informatii baza de date
 
- $AdresaBazaDate = "dbs.gameszone.ro";
- $UtilizatorBazaDate = "novuslink";
- $ParolaBazaDate = "stropscinortcele";
- $NumeBazaDate = "novuslink";
+ $AdresaBazaDate = "";
+ $UtilizatorBazaDate = "";
+ $ParolaBazaDate = "";
+ $NumeBazaDate = "";
 
  $conexiune = mysql_connect($AdresaBazaDate,$UtilizatorBazaDate,$ParolaBazaDate) or die("Nu ma pot conecta la MySQL!");
  mysql_select_db($NumeBazaDate, $conexiune) or die("Nu gasesc baza de date !");
